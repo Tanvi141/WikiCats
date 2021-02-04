38312e4d682c0469fd0d6030ac6f445bc15bfd20
Tanvi Karandikar <tanvi.karandikar141@gmail.com>
2021-02-04 10:28:43 +0530

    cat score

--- src/parse_tree/WikiParser.py
+++ src/parse_tree/WikiParser.py
@@ -1,3 +1,4 @@
+import statistics
 from Tree import cattree, articletree
 from ArticleMap import articlemap
 
@@ -12,6 +13,8 @@ class WikiParser():
     def compare_dicts(self, dict1, dict2, to_remove_start, to_remove_end, dirn, ht=0): 
         #pass to_remove_* as 0 if you want to remove, else pass as none
         to_ret = []
+        score_components = []
+
         for node1 in dict1.keys():
             for node2 in dict2.keys():
 
@@ -21,20 +24,25 @@ class WikiParser():
                             continue
                         for occur_1 in dict1[node1]:
                             for occur_2 in dict2[node2]:
+                                eles = []
                                 if dirn == 12:
                                     to_ret.append((dirn, node1, node2, occur_1[ht], occur_2[ht])) #dirn of edge, node in s1, node in s2
                                 else:
                                     to_ret.append((dirn, node2, node1, occur_2[ht], occur_1[ht])) 
+                                eles.append(\
+                                    abs(occur_1[ht]) + abs(occur_2[ht]) \
+                                )
+                        try:
+                            score_components.append(statistics.mean(eles))
+                        except:
+                            pass
+        
+        try:
+            return [to_ret, statistics.mean(score_components)]
+        except:
+            return [to_ret, 100000]
 
-                # for node2_adjac in self.cattree.adjlist[node2]:
-                #     if node1 == node2_adjac[0]:
-                #         if dict1[node1][0] == to_remove_end or dict2[node2][0] == to_remove_start:
-                #             continue
-                #         to_ret.append((-1, node1, node2)) #can change to 0 or 1            
-                
-        return to_ret
-
-    def compare_two_cats(self, cat1, cat2): 
+    def get_cat_score(self, cat1, cat2): 
         
         #subtree 1
         cat1_parents = self.cattree.get_neighbours(cat1, 2, "parents")
@@ -45,24 +53,46 @@ class WikiParser():
 
         # n1, n2 are siblings, start is CA
         p_pn = self.compare_dicts(cat1_parents, cat2_parents, 0, None, 12)
-        p_pn += self.compare_dicts(cat2_parents, cat1_parents, 0, None, 21)
-
+        temp = self.compare_dicts(cat2_parents, cat1_parents, 0, None, 21)
+        p_pn[0] += temp[0]
+        p_pn[1] += temp[1]
+ 
         # labelless relations
         np_c = self.compare_dicts(cat1_parents, cat2_children, None, 0, 12)
-        np_c += self.compare_dicts(cat2_parents, cat1_children, None, 0, 21)
+        temp = self.compare_dicts(cat2_parents, cat1_children, None, 0, 21)
+        np_c[0] += temp[0]
+        np_c[1] += temp[1]
 
         c_c = self.compare_dicts(cat1_children, cat2_children, 0, 0, 12)
-        c_c += self.compare_dicts(cat2_children, cat1_children, 0, 0, 21)
+        temp = self.compare_dicts(cat2_children, cat1_children, 0, 0, 21)
+        c_c[0] += temp[0]
+        c_c[1] += temp[1]
 
         # parent-child
         nc_pn = self.compare_dicts(cat1_children, cat2_parents, None, None, 12)
-        nc_pn += self.compare_dicts(cat2_children, cat1_parents, None, None, 21)
+        temp = self.compare_dicts(cat2_children, cat1_parents, None, None, 21)
+        nc_pn[0] += temp[0]
+        nc_pn[1] += temp[1]
 
         print(p_pn,"\n\n")
         print(np_c,"\n\n")
         print(c_c,"\n\n")
         print(nc_pn,"\n\n")
 
+        cat_score = p_pn[1] + np_c[1] + c_c[1] + nc_pn[1]
+        
+        return cat_score
+
+    def get_art_score(self, cat1, cat2):
+        return 0
+
+    def compare_two_cats(self, cat1, cat2):
+        cat_score = self.get_cat_score(cat1, cat2)
+        art_score = self.get_art_score(cat1, cat2)
+
+        sk_score = cat_score + art_score
+        print(sk_score)
+        return sk_score
 
 wikiparser = WikiParser(cattree, articletree, articlemap)
 wikiparser.compare_two_cats(50624508, 38398308)
