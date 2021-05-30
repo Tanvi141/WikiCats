2be84cfd41ab472ddd792eda8fc227861e887302
Mallika Subramanian <mallika.subramanian@students.iiit.ac.in>
2021-05-30 22:47:20 +0530

    Modify category vector Open KE script

--- src/parse_tree/cat_vec.py
+++ src/parse_tree/cat_vec.py
@@ -1,21 +1,32 @@
 import json
 import tqdm
+import requests
 
 id2vec = {}
 cat_ids = set()
+art_ids = set()
+found_cat_ids = set()
 
-#load the category IDs for all cat under Union Territories
-with open("../../data/article_id_name.txt") as f:
+#load the category IDs for all articles  under Union Territories
+#with open("../../data/article_id_name.txt") as f:
     
-    line = f.readline().strip()
+#    line = f.readline().strip()
 
-    while(line):
-        id, _ = line.split(":", 1)
-        cat_ids.add(int(id))
+#    while(line):
+#        id, _ = line.split(":", 1)
+#        art_ids.add(int(id))
 
-        line = f.readline().strip()
+#        line = f.readline().strip()
 
-with open("../../../WikipediaClean5Negative300Skip10/WikipediaClean5Negative300Skip10.txt") as f:
+with open("../../data/cat2kg_id.json") as f:
+    data = json.load(f)
+    cat_ids = set(data.values())
+    print(len(cat_ids))
+    for key, val in data.items():
+        if val == "Q47038375":
+            print(key) 
+
+with open("/scratch/mallika/Wikidata/Wikidata/knowledge graphs/entity2id.txt") as f:
 
     line_count = 0
     vec_count = 0
@@ -23,29 +34,23 @@ with open("../../../WikipediaClean5Negative300Skip10/WikipediaClean5Negative300S
     word_key_count = 0
     line = f.readline().strip() #ignoring the header line
     line = f.readline().strip()
-    
+
     while(line):
         
         line_count += 1
 
-        if line_count % 10000 == 0:
+        if line_count % 1000000 == 0:
             print("Read", line_count, "line | UT cats so far", cat_vec_count)
 
         vec_count += 1
-        id, vec = line.split(" ", 1)
-
-        #check if the Key is an ID or a word
-        try:
-            id = int(id)
-        except:
-            word_key_count += 1
-            line = f.readline().strip()
-            continue
+        id, vec = line.split("\t", 1)
         
         if id in cat_ids:
             if id not in id2vec:
+                id2vec[id] = 2
                 # id2vec[id] = [float(val) for val in (vec.split(" "))] 
                 cat_vec_count += 1
+                found_cat_ids.add(id)
 
         line = f.readline().strip()
 
@@ -53,6 +58,8 @@ print("\n---------------------------------------------")
 print("Total vectors: ", vec_count)
 print("Total Cat vecs: ", cat_vec_count)
 print("Total Word Keys: ", word_key_count)
-        
+
+diff = cat_ids.difference(found_cat_ids)        
+print(diff)
 
 
